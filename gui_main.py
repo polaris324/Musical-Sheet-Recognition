5932e6003eb6ef9610cfa6f5cac382fea086ec4d
D0746265 <paul776618@gmail.com>
2021-12-04 01:44:35 +0800

    GUI localization
    gui_main.py
    - Translate to Chinese
    - Add fetch note button
    - Binarization on change
    
    Add Logo_32.ico
    Add Logo_128.ico
    Add Logo_256.ico

--- gui_main.py
+++ gui_main.py
@@ -31,16 +31,7 @@ def openByFilePath():
     # print(file_loc)# test
     
     showImage(file_location)
-    # img = Image.open(file_loc)
-    # canvas = tk.Canvas(ws, width=img.width // 5, height=img.height // 5, bg='gray')
-    # img = img.resize( (img.width // 5, img.height // 5) )
-    # image_file =  ImageTk.PhotoImage(img)
-        
-    # label = tk.Label(image = image_file)
-    # label.grid(column=1, row=1 , padx=10, pady=10)
     #https://stackoverflow.com/questions/66159973/display-image-when-button-is-clicked-python-gui
-    
-    # return
 
 def Upload():
     path = filedialog.askopenfilename()
@@ -48,76 +39,6 @@ def Upload():
     testText = path
     print(path)
     showImage(path)
-
-def mainA():
-    statusText.set('Status : Running')
-    filename = testText
-    
-    statusText.set('Status : Running - preprocessing')
-    reload()
-    findline._changeThersholdType(threshold.get(), thValue)
-    thresh, imgmark = findline.findline(filename)
-    
-    statusText.set('Status : Running - line distance & thickness')
-    reload()
-    fiveline, lastx = five(imgmark)
-    
-    statusText.set('Status : Running - getting Symbol mapping')
-    reload()
-    staffRow = get_fiveline_rows(fiveline, lastx)
-    staffRow_spacing, line_spacing = get_rows_dist(staffRow)
-    
-    global mapSymbol
-    mapSymbol = getSymbol(fiveline, thresh, staffRow, staffRow_spacing, lastx)
-    showThImage()
-    try:
-        noteheight._changeKey(keysValue.get()[0].upper())
-    except IndexError:
-        noteheight._changeKey('C')
-    showImage(testText)
-    statusText.set('Status : Running - preprocessing done')
-    
-    # statusText.set('Status : Running - CNN')
-    # reload()
-    # noteH = noteheight.noteheight(mapSymbol)
-    # noteL = noteLength(mapSymbol)
-    
-    # statusText.set('Status : Running - creating Music')
-    # reload()
-    # addmusic._changeBeat(tempo.get())
-    # addmusic.addmusic(noteL, noteH)
-    # statusText.set('Status : Done')
-    
-def mainB():
-    global mapSymbol
-    
-    statusText.set('Status : Running - CNN')
-    reload()
-    noteH = noteheight.noteheight(mapSymbol)
-    noteL = noteLength(mapSymbol)
-    
-    statusText.set('Status : Running - creating Music')
-    reload()
-    addmusic._changeBeat(tempo.get())
-    addmusic.addmusic(noteL, noteH)
-    statusText.set('Status : Done')
-        
-def reload():
-    ws.update()
-    
-def showThImage():
-    image_width = Image.open("Binarizated Img.jpg").width
-    image_height = Image.open("Binarizated Img.jpg").height
-    if ( max(image_height, image_width) == image_height ):# vertical image
-        resize_height = 600
-        resize_width = 600/image_height*image_width
-    else:# horizon image
-        resize_height = 450/image_width*image_height
-        resize_width = 450
-    image_file = ImageTk.PhotoImage(Image.open("Binarizated Img.jpg").resize((int(resize_width)+20, int(resize_height)+20)))
-    
-    canvas_2.image = image_file
-    canvas_2.create_image(225,300,anchor='center',image = image_file)
 
 def showImage(file_location):    
     image_width = Image.open(file_location).width
@@ -134,33 +55,121 @@ def showImage(file_location):
     canvas.create_image(225,300,anchor='center',image = image_file)
     global haveImage
     haveImage = True
-    statusText.set('Status : Ready')
+    statusText.set("狀態 : 圖片已載入")
+    
+def mainA():
+    statusText.set("狀態 : 執行中...")
+    filename = testText
+    
+    reload()
+    findline._changeThersholdType(threshold.get(), thValue)
+    thresh, imgmark = findline.findline(filename)
+    
+    reload()
+    fiveline, lastx = five(imgmark)
+    
+    statusText.set("狀態 : 符號擷取中...")
+    reload()
+    staffRow = get_fiveline_rows(fiveline, lastx)
+    staffRow_spacing, line_spacing = get_rows_dist(staffRow)
+    
+    global mapSymbol
+    mapSymbol = getSymbol(fiveline, thresh, staffRow, staffRow_spacing, lastx)
+    showThImage("test.jpg")
+    try:
+        noteheight._changeKey(keysValue.get()[0].upper())
+    except IndexError:
+        noteheight._changeKey('C')
+    
+    statusText.set("狀態 : 符號擷取完成")
+    
+    # statusText.set('Status : Running - CNN')
+    # reload()
+    # noteH = noteheight.noteheight(mapSymbol)
+    # noteL = noteLength(mapSymbol)
+    
+    # statusText.set('Status : Running - creating Music')
+    # reload()
+    # addmusic._changeBeat(tempo.get())
+    # addmusic.addmusic(noteL, noteH)
+    # statusText.set('Status : Done')
+    
+def mainB():
+    global mapSymbol
+    
+    statusText.set("狀態 : 符號辨識中...")
+    reload()
+    noteH = noteheight.noteheight(mapSymbol)
+    noteL = noteLength(mapSymbol)
+    
+    statusText.set("狀態 : 音樂生成中...")
+    reload()
+    addmusic._changeBeat(tempo.get())
+    addmusic.addmusic(noteL, noteH)
+    statusText.set("狀態 : 音樂生成完畢")
+        
+def reload():
+    ws.update()
+    
+def showThImage(which_file):
+    image_width = Image.open(which_file).width
+    image_height = Image.open(which_file).height
+    if ( max(image_height, image_width) == image_height ):# vertical image
+        resize_height = 600
+        resize_width = 600/image_height*image_width
+    else:# horizon image
+        resize_height = 450/image_width*image_height
+        resize_width = 450
+    image_file = ImageTk.PhotoImage(Image.open(which_file).resize((int(resize_width)+20, int(resize_height)+20)))
+    
+    canvas_2.image = image_file
+    canvas_2.create_image(225,300,anchor='center',image = image_file)
+
 
 def showTempoValueBySelect(event):
      #print("New Element Selected")# test
      print(tempo.get())# test
-     tempoText.set('Tempo of the sheet : ' + tempo.get())
+     tempoText.set("MIDI節拍單位 :" + tempo.get())
      
 def getTempoValue():
      #print("New Element Entered")# test
      print(tempo.get())# test
-     tempoText.set('Tempo of the sheet : ' + tempo.get())
+     tempoText.set("MIDI節拍單位 :" + tempo.get())
      global haveImage
      if haveImage:
-         statusText.set('Status : Ready')
+         statusText.set("狀態 : 參數已變更(MIDI節拍單位 : "+ tempo.get() + " )")
+     else:
+         statusText.set("狀態 : 參數已變更[請輸入圖片]")
      #play_music(music_file)
 
 def showThresholdValueBySelect(event):
      #print("New Element Selected")# test
      print(threshold.get())# test
-     thresholdTypeText.set('Threshold type :' + threshold.get())
-     if threshold.get() == " Customize":
+     thresholdTypeText.set("二值化方法 :" + threshold.get())
+     if threshold.get() == " 自定義閥值":
          thresholdValue.grid()
      else:
          thresholdValue.grid_remove()
      global haveImage
      if haveImage:
-         statusText.set('Status : Ready')
+         statusText.set("狀態 : 參數已變更(二值化方法 : "+ threshold.get() + " )")
+     else:
+        statusText.set("狀態 : 參數已變更[請輸入圖片]")
+     import time
+     reload()
+     time.sleep(1.5)
+     statusText.set("狀態 : 二值化執行中...")
+     filename = testText
+    
+     reload()
+     findline._changeThersholdType(threshold.get(), thValue)
+     thresh, imgmark = findline.findline(filename)
+    
+     reload()
+     fiveline, lastx = five(imgmark)
+     
+     showThImage("five.jpg")
+     statusText.set("狀態 : 二值化完成")
     
 
 def getScaleValue(event):
@@ -172,25 +181,47 @@ def getScaleValue(event):
         thValue = w.get()
     global haveImage
     if haveImage:
-        statusText.set('Status : Ready')
+        statusText.set("狀態 : 參數已變更\n(二值化-自定義方法之參數 : "+ (str(thValue)) + " )")
+    else:
+        statusText.set("狀態 : 參數已變更[請輸入圖片]")
+    import time
+    reload()
+    time.sleep(1.5)
+    statusText.set("狀態 : 二值化執行中...")
+    filename = testText
+    
+    reload()
+    findline._changeThersholdType(threshold.get(), thValue)
+    thresh, imgmark = findline.findline(filename)
+    
+    reload()
+    fiveline, lastx = five(imgmark)
+     
+    showThImage("five.jpg")
+    statusText.set("狀態 : 二值化完成")
      
 def getKeysValue(event):
     w = event.widget
+    KEYS = "ABCDEFG"
     if isinstance(w, tk.Entry):
-        print (w.get())
-        keysText.set('Keys : ' + keysValue.get()[0].upper())
+        if(KEYS.find(keysValue.get()[0].upper()) != -1):
+            print (w.get())
+            keysText.set("調性 : " + keysValue.get()[0].upper())
+        else:
+            keysText.set("調性 : 輸入不正確\n[請輸入正確調性(介於A~G不分大小寫)]")
     global haveImage
     if haveImage:
-        print(haveImage)
-        statusText.set('Status : Ready')
+        statusText.set("狀態 : 參數已變更(調性 : "+ keysValue.get()[0].upper() + " )")
+    else:
+        statusText.set("狀態 : 參數已變更[請輸入圖片]")
 #-----------------------------------------------------------------------------------------------
 
 
 # Initial window setting
 ws = tkd.Tk()
 ws.title("Musical-Sheet-Recognition")
-ws.geometry("1600x750")
-#ws.iconbitmap("Downloads\icon.ico")
+ws.geometry("1450x750")
+ws.iconbitmap("Logo_256.ico")
 ws.config(bg="lightgray")
 
 # Create Frame for Canvas
@@ -199,10 +230,12 @@ canvas = tk.Canvas(frame, width=450, height=600)
 canvas.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
 
 frame.grid(column=1, row=1, columnspan=3, rowspan=16)
+tk.Label(ws, text="輸入圖像", bg="lightgray").grid(column=1, row=17, columnspan=3)
 
 # Drag and drop setting
 var = tk.StringVar()
-tk.Label(ws, text='Path of the Folder :',bg="lightgray").grid(column=0, row=0, padx=10, pady=10, sticky=tk.N)
+var.set("請拖拉文件至此")
+tk.Label(ws, text="文件位置 :",bg="lightgray").grid(column=0, row=0, padx=10, pady=10, sticky=tk.N)
 # EnterBox
 e_box = tk.Entry(ws, textvar=var, width=45)
 e_box.grid(column=1, row=0 , padx=10, pady=10, sticky=tk.N)
@@ -210,17 +243,17 @@ e_box.drop_target_register(tkd.DND_FILES)
 e_box.dnd_bind('<<Drop>>', drop)
 
 # SendFilePath Button
-btn_upload = tk.Button(ws, text='Send', command=openByFilePath)#https://stackoverflow.com/questions/13099908/python-tkinter-return-value-from-function-used-in-command
+btn_upload = tk.Button(ws, text="開啟", command=openByFilePath)#https://stackoverflow.com/questions/13099908/python-tkinter-return-value-from-function-used-in-command
 btn_upload.grid(column=2, row=0, padx=10, pady=5)
 
 # Upload Button
-btn_upload = tk.Button(ws, text='Upload', command=Upload)
+btn_upload = tk.Button(ws, text="選擇文件", command=Upload)
 btn_upload.grid(column=3, row=0, padx=10, pady=5)
 haveImage = False
 
 #DirectEnter/SelectBox for Speed Selection
 tempoText = tk.StringVar()
-tempoText.set('Tempo of the sheet :')   
+tempoText.set("MIDI節拍單位 :")   
 tk.Label(ws, textvariable=tempoText,bg="lightgray", anchor=tk.W, width=32).grid(column=4, row=0, columnspan=3)
 '''
 values=["Larghissimo -20",
@@ -242,14 +275,14 @@ tempo = tk.StringVar()
 tk.Entry(ws, textvariable = tempo).grid(column=4, row=1, pady=10, columnspan=3,sticky='NWE')
 # combo.bind("<<ComboboxSelected>>", showTempoValueBySelect)
 #combo.grid(column=4, row=1, pady=10, columnspan=3,sticky='NWE')
-tk.Button(ws, text='Set Tempo', command=getTempoValue).grid(column=7, row=1, padx=10, pady=10, sticky=tk.N)
+tk.Button(ws, text="設定", command=getTempoValue).grid(column=7, row=1, padx=10, pady=10, sticky=tk.N)
 
 # Bar for threshold
 thresholdTypeText = tk.StringVar()
-thresholdTypeText.set('Threshold type :')
+thresholdTypeText.set("二值化方法 :")
 tk.Label(ws, textvariable=thresholdTypeText, bg="lightgray").grid(column=4, row=2, columnspan=3, sticky=tk.W)
 values=[" OTSU",
-        " Customize",
+        " 自定義閥值",
         " YEN"]
 threshold = tk.StringVar()
 thresholdTypeCombo = ttk.Combobox(ws, value = values, textvariable=threshold)
@@ -257,13 +290,13 @@ thresholdTypeCombo.bind("<<ComboboxSelected>>", showThresholdValueBySelect)
 thresholdTypeCombo.grid(column=4, row=3, columnspan=3, sticky='WES')
 thValue = tk.StringVar()
 thresholdValue = tk.Scale(ws, from_=1, to=254,orient='horizonta' ,tickinterval=50,length=200)
-thresholdValue.grid(column=4, row=5, rowspan=3, columnspan=3, padx=10, pady=5)
+thresholdValue.grid(column=4, row=4, rowspan=4, columnspan=3, padx=10, pady=5)
 thresholdValue.bind("<ButtonRelease>", getScaleValue)
 thresholdValue.grid_remove()
 
 # Keys
 keysText = tk.StringVar()
-keysText.set('Keys :')
+keysText.set("調性 :")
 tk.Label(ws, textvariable=keysText, bg="lightgray").grid(column=4, row=8, columnspan=3, sticky=tk.W)
 keysValue = tk.StringVar()
 keysEntry = tk.Entry(ws, textvariable = keysValue)
@@ -272,12 +305,12 @@ keysEntry.grid(column=4, row=9, pady=10, columnspan=3,sticky='NWE')
 
 # Submit
 statusText = tk.StringVar()
-statusText.set('Status :')
-tk.Label(ws, textvariable=statusText, bg="lightgray").grid(column=4, row=13, columnspan=3, sticky=tk.W)
-Confirm = tk.Button(ws, text='Confirm', command=mainA)
-Confirm.grid(column=4, row=14, columnspan=3, padx=10, pady=5,sticky = 'we')
-Run = tk.Button(ws, text='RUN', command=mainB)
-Run.grid(column=4, row=15, columnspan=3, padx=10, pady=5,sticky = 'we')
+statusText.set("狀態 :")
+tk.Label(ws, textvariable=statusText, bg="lightgray").grid(column=4, row=10, columnspan=3, sticky=tk.W)
+Confirm = tk.Button(ws, text="進行音符擷取", command=mainA)
+Confirm.grid(column=4, row=11, columnspan=3, padx=10, pady=5,sticky = 'we')
+Run = tk.Button(ws, text="執行樂譜辨識", command=mainB)
+Run.grid(column=4, row=12, columnspan=3, padx=10, pady=5,sticky = 'we')
 
 # Music player
 pygame.mixer.init()
@@ -295,9 +328,10 @@ def ex():
     pygame.mixer.music.stop()
     ws.destroy()
 
-Play = tk.Button(ws, text = 'Play', command = play).grid(column=4, row=16, padx=10, pady=5, sticky='swe')
+tk.Label(ws, text="播放控制 :", bg="lightgray").grid(column=4, row=15, columnspan=3, sticky=tk.W)
+Play = tk.Button(ws, text = "播放", command = play).grid(column=4, row=16, padx=10, pady=5, sticky='swe')
 # Pause = tk.Button(ws,text = 'Pause', command = pause).grid(column=5, row=16, padx=10, pady=5, sticky='swe')
-Stop = tk.Button(ws, text="Stop", command=stop).grid(column=6, row=16, padx=10, pady=5, sticky='swe')
+Stop = tk.Button(ws, text="停止", command=stop).grid(column=6, row=16, padx=10, pady=5, sticky='swe')
 
 #Threshold Image
 thImg = tk.Frame(ws)
@@ -305,10 +339,11 @@ canvas_2 = tk.Canvas(thImg, width=450, height=600)
 canvas_2.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
 
 thImg.grid(column=8, row=1, columnspan=3, rowspan=16)
+tk.Label(ws, text="預處理結果", bg="lightgray").grid(column=8, row=17, columnspan=3)
 
 
 #Exit Button
-quit_buttom = tk.Button(ws, text="Exit Program", command=ex)
+quit_buttom = tk.Button(ws, text="離開程式", command=ex)
 quit_buttom.grid(column=11, row=17, padx=10, pady=5)
 
 #-----------------------------------------------------------------------------------------------
