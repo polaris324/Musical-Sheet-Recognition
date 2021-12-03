50b46ed55b36c99b0db6440d00acb0ee4001526f
Lee Huan Liang <huanlianglee@gmail.com>
2021-12-04 02:06:16 +0800

    Update catchSymbol v2.0 & Integrat old doc
    Update catchSymbol v2.0
    - change catchSymbol method with HistScan method
    - Warning: This commit is without GUI & addmusic features, if you need to use a function that is not currently available, you can copy the files from the folder CNN_NN_addmusic but the features inside are not up to date.
    
    Integrat old doc
    - Integration all the old document to folder old_catchSymbol

---
+++ get_axis_line.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+import cv2
+import numpy as np
+from scipy import stats
+# from getSymbol import getSymbol
+# import time
+
+"""test header"""
+# from pre_processing import preprocessing
+# img = cv2.imread('1.png', cv2.IMREAD_GRAYSCALE)
+# h, w = img.shape  # Image Length & Width
+
+# thresh, h, w = preprocessing(img, h, w)
+
+
+def findstaff_spacing(thresh, h, w):
+    
+    lineAxis = [] # Catch all related line axis
+    threshOfLine = np.multiply(w, 0.6).astype(int) # set thresh line of staffline
+    
+    numAryOfBlack = np.count_nonzero(thresh==0, axis=1) # get Hist of each row
+    
+    distCnt = 0 # counter for each spacing
+    
+    for row in range(h):    # if match staffline, get axis
+        if numAryOfBlack[row] > threshOfLine and distCnt != 0:
+            lineAxis.append([row, distCnt])
+            distCnt = 0
+        else:
+            distCnt += 1
+    spacing = stats.mode(lineAxis)[0][0][1]
+    
+    lineCnt = 0
+    flag_findfive = 0
+    # Exception_staffLine = 0
+    lineRow = []
+    staffRow = []
+    
+    for row in range(len(lineAxis)):
+        if(flag_findfive):
+            if(    (lineAxis[row][1] - spacing) >= -1 
+               and (lineAxis[row][1] - spacing) <=  1): # is staffline, spacing is +-1
+                lineRow.append(lineAxis[row][0])
+                lineCnt += 1
+                # print("found staff line", lineRow)
+            elif(lineAxis[row][1] == 0):                # is overlapping, pop last & add new staffline
+                lineRow.pop(lineCnt)
+                lineRow.append(lineAxis[row][0])
+                # print("found overlapping", lineRow)
+            # elif(lineCnt != 0):
+                # Exception_staffLine = 1
+    
+            if(lineCnt == 4):                           # if found five staffline, then add to one group
+                staffRow.append(lineRow)
+                lineRow = []
+                lineCnt = 0
+                flag_findfive = 0
+        else:                                           # catch flag if first staffline for each group
+            flag_findfive = 1
+            lineRow.append(lineAxis[row][0])
+            
+    imgmask = np.zeros([h, w], dtype="uint8")   # comfirm staffline generate
+    lastX = w
+    
+    # if raise exception, need to return a callout value
+    
+    # output gen comfirm staffline
+    
+    for each in range(len(staffRow)):
+        for row in range(5):
+            lastCheck_flag = 1
+            for col in range(w-1,0,-1):
+                if thresh[staffRow[each][row]][col] == 0:
+                    imgmask[staffRow[each][row]][col] = 255
+                    
+                    if(row == 4 and lastCheck_flag and col <= lastX and
+                       thresh[staffRow[each][row]-1][col-1] == 255):
+                        # print(staffRow[each][row], col)
+                        lastX = col
+                        lastCheck_flag = 0
+                    
+    # check monophony or not
+    monophony = 0
+    for row in range(staffRow[0][4], staffRow[1][0] + spacing, 1):
+        if(thresh[row][lastX+1] == 0):
+            monophony = 1
+        else:
+            monophony = 0
+            break
+    
+    cv2.imwrite("imgmask.jpg", imgmask) # if output test img
+    
+    # if raise exception, need to return a callout value
+    return imgmask, staffRow, spacing, lastX, monophony #, lineAxis
+
+"""test main"""
+# imgmask, staffRow, spacing, lastX, mono, Arr = findstaff_spacing(thresh, h, w)
+
+# "exec_time set"
+# exec_time = []
+# "exec_time scan"
+# a = time.time()
+# "exec_time stop"
+# b = time.time()
+# exec_time.append( b - a )
+# print('Method', exec_time[0])
---
+++ main.py
@@ -0,0 +1,31 @@
+from pre_processing import preprocessing
+from get_axis_line import findstaff_spacing
+from getSymbol import getSymbol
+# from noteheight import noteheight
+# from NotationByNN import noteLength
+# from addmusic import addmusic
+from cv2 import imread, IMREAD_GRAYSCALE
+# import os
+# import time
+
+# In[Read Image]
+filename = input() # maybe use .argparse to avoid error input
+img = imread(filename, IMREAD_GRAYSCALE)
+h, w = img.shape  # Image Length & Width
+
+# In[Image Preprocessing]
+thresh, h, w = preprocessing(img, h, w)
+
+# In[Staff Detection, Reference line distance & thickness]
+imgmask, staffRow, spacing, lastX, mono = findstaff_spacing(thresh, h, w)
+
+# In[GET NODE]
+mapSymbol = getSymbol(imgmask, thresh, staffRow, spacing, lastX, mono)
+
+# In[Get Note High & Note Long]
+
+# noteH = noteheight(mapSymbol)
+# noteL = noteLength(mapSymbol)
+
+# In[Create Music]
+# addmusic(noteL, noteH)
